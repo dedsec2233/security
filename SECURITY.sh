8a866f3ceca57e25e38eeae2f6e7592207e398db
dedsec2233 <69725203+dedsec2233@users.noreply.github.com>
2020-08-16 06:47:38 +0700

    Create SECURITY.sh

---
+++ SECURITY.sh
@@ -0,0 +1,7 @@
+echo "# HACK-CCTV" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:dedsec2233/HACK-CCTV.git
+git push -u origin master
+                
