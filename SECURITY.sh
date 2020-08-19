b919bbaf85a763adfac7f8c399f6e3cd55d367b4
dedsec2233 <69725203+dedsec2233@users.noreply.github.com>
2020-08-19 16:30:12 +0700

    Delete SECURITY.sh

--- SECURITY.sh
+++
@@ -1,7 +0,0 @@
-echo "# HACK-CCTV" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:dedsec2233/HACK-CCTV.git
-git push -u origin master
-                
